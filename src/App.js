13188f84b1f07e1f16114f170f1a55234f93c77e
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-07 23:57:26 +0100

    writing initial code for useReducer

--- src/App.js
+++ src/App.js
@@ -39,20 +39,3 @@ function App() {
 }
 
 export default App;
-
-//useReducer - basic structure:
-/* const [state, dispatchFn] = useReducer(reducerFn, intialState, initFn) */
-
-// Explanation of function above:
-// left hand side of function
-/* state snapshot = used in the component re-render/ re-evaluation cycle */
-/* dispatchFn -> can be used to dispatch a new action (i.e. trigger an an update of the state) */
-/* this first part -> similar as for useState() -> though the state updating function will work
-differently. */
-/* Instead of just setting a new state value, you will dispatch an action -> and that action will
-be consumed by the first argument you pass to useReducer - a so-called reducer function. */
-
-// right hand side of function
-/* preducerFn -> (prevState, action) => newState. 
-A function that is triggered that is triggered automatically once an action is dispatched (via 
-dispatchFun()) - it recieves the latest state snapshot and should return the new updated state. */
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,48 @@
-import React, { useState, useEffect } from "react";
+//useReducer - basic structure:
+/* const [state, dispatchFn] = useReducer(reducerFn, intialState, initFn) */
+
+// Explanation of function above:
+// left hand side of function
+/* state snapshot = used in the component re-render/ re-evaluation cycle */
+/* dispatchFn -> can be used to dispatch a new action (i.e. trigger an an update of the state) */
+/* this first part -> similar as for useState() -> though the state updating function will work
+differently. */
+/* Instead of just setting a new state value, you will dispatch an action -> and that action will
+be consumed by the first argument you pass to useReducer - a so-called reducer function. */
+
+// right hand side of function
+/* preducerFn -> (prevState, action) => newState. 
+A function that is triggered that is triggered automatically once an action is dispatched (via 
+dispatchFun()) - it recieves the latest state snapshot and should return the new updated state. */
+/* In addition, you can also set some initial state and also an initial function, in case your 
+initial state is a bit more complex. A function to set the initial state programmatically. And for example,
+the result of lets say HTTP requests */
+
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+/* arrow function stored in this emailReducer const for the useReducer() function below. */
+const emailReducer = () => {};
+
 const Login = (props) => {
+  /* could use useReducer() to combine our entered values and validities for the email and the password. 
+  Goal is to combine the value and the validity into one state managed by useReducer. */
   const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  /* Calling useReducer() function. useReducer return an array with exactly two elements, and we can
+  use array destructuring to pull those elements out of it. */
+  /* Going to utilise useReducer for my email and my password seperately. Get back emailState, and 
+  dispatchEmail (our dispatch function). These two names are up to me. */
+  /* Then I mentioned that useReducer as a first argument takes a function. Using outsourced name 
+  function (written above). emailReducer function now pointed at from above. */
+  const [emailState, dispatchEmail] = useReducer(emailReducer);
 
   useEffect(() => {
     console.log("EFFECT RUNNING");

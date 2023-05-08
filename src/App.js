46d50df96cae2c1a941e21030022e303ac48297f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-08 18:04:17 +0100

    comments on why props might get complicated -> passing lots of data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  //Introducing React Context
+  /* Problem -> passing a lot of data through a lot of components via props -> good example of this 
+  is our 'isLoggedIn' state and the login functions. */
+  /* We're managing this state in the app component, because I lifted this state up to that component.
+  We need this isLoggedIn state and the functions to change it everywhere in this application. */
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -29,8 +34,19 @@ function App() {
 
   return (
     <React.Fragment>
+      {/* We use the isLoggedIn state in the header, to which I'm passing it through the 
+      isAuthenticated prop. From there we are able to logout -> so I passed the pointer at 
+      the logoutHandler to my header on the onLogout prop. */}
+      {/* here, we're passing isLoggedIn through the isAuthenticated prop to the MainHeader.
+      Passing the logout handler through the onLogout prop to the main header as well. */}
+      {/* This isn't even being utilised in the MainHeader component though, its being used in 
+      the Navigation component. So -> in MainHeader I'm recieving props, (data through props) 
+      which I'm not actually using primarily in the main header, instead, I just forward that data. */}
+      {/* Bigger apps -> chains might get longer and longer */}
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
+        {/* Also need to the login state to render different content here -> either the login 
+        or the home component, and to those components */}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>

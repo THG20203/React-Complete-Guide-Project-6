4a4693835a05208b27fbab0b2b6b3da8eb7e9cd2
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-08 18:28:40 +0100

    first utilisation of context api code

---
+++ src/store/auth-context.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+/* On react, going to call createContext -> which creates a Context object */
+/* createContext takes a default context and Context here is just your App or component 
+white State. Up to you what that state should be. Could be something simple like my string, 
+my State. */
+React.createContext("My state");

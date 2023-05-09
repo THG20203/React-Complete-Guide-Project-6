778176917c0673f66e19987dc63663fd62f33021
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-09 22:19:39 +0100

    moving logic to auth-context and adding logout button

--- src/App.js
+++ src/App.js
@@ -1,84 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
-import AuthContext from "./store/auth-context";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  //Introducing React Context
-  /* Problem -> passing a lot of data through a lot of components via props -> good example of this 
-  is our 'isLoggedIn' state and the login functions. */
-  /* We're managing this state in the app component, because I lifted this state up to that component.
-  We need this isLoggedIn state and the functions to change it everywhere in this application. */
-
-  //REACT CONTEXT EXPLANATION:
-  /* React context -> component-wide, behind the scenes state storage. This allows us to trigger an 
-  action in that component wide state storage, then directly pass that to the component that is interested
-  without building such long prop chains. */
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-
-    if (storedUserLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
-    /* EXPLANATION OF REACT CONTEXT STATE USAGE */
-    /* We can wrap our main header and the main section -> main html element with Auth-Context */
-    /* AuthContext itself -> not a component -> in JSX we need a component though. But with a . a dot
-    we can access a property on this Auth-Context object that contains a component and thats the 
-    '.Provider' */
-    /* Now AuthContext is wrapped around these components, all these components <MainHeader> and <main>
-    and all their descendent components (so all their children and their childrens children etc.) - all 
-    those components will now have access to that context. AuthContext can also become a root level 
-    component - remove ReactFragment. */
-
-    //MAKING CONTENT DYNAMIC
-    /* We can set up dynamic context -> don't just pass data to our components but also functions. On 
-    AuthContext provider as well as passing down isLoggedIn, pass down onLogout -> can point at 
-    the logoutHandler */
-    /* If I do that, every listening component -> every component that listens to Auth context will be 
-    able to utilise logoutHandler simply through the onLogout context value. */
-    <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
-    >
-      {/* EXPLANATION OF PROPS PROBLEM AND THEREFORE THE NEED FOR REACT CONTEXT API */}
-      {/* We use the isLoggedIn state in the header, to which I'm passing it through the 
-      isAuthenticated prop. From there we are able to logout -> so I passed the pointer at 
-      the logoutHandler to my header on the onLogout prop. */}
-      {/* here, we're passing isLoggedIn through the isAuthenticated prop to the MainHeader.
-      Passing the logout handler through the onLogout prop to the main header as well. */}
-      {/* This isn't even being utilised in the MainHeader component though, its being used in 
-      the Navigation component. So -> in MainHeader I was recieving props, (data through props) 
-      which I'm not actually using primarily in the main header, instead, I just forward that data. */}
-      {/* Bigger apps -> chains might get longer and longer */}
+    <React.Fragment>
       <MainHeader />
       <main>
-        {/* Also need to the login state to render different content here -> either the login 
-        or the home component, and to those components */}
-        {/* Here in the main section, I will still pass down the logoutHandler through onLogout 
-        and the loginHandler through onLogin because I directly use those handlers in the Login and 
-        Home components. */}
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,6 +1,18 @@
+/* make auth-context stand alone file -> manages entire login state in AuthContextProvider component */
+//Introducing React Context
+/* Problem -> passing a lot of data through a lot of components via props -> good example of this 
+  is our 'isLoggedIn' state and the login functions. */
+/* We're managing this state in the app component, because I lifted this state up to that component.
+  We need this isLoggedIn state and the functions to change it everywhere in this application. */
+
+//REACT CONTEXT EXPLANATION:
+/* React context -> component-wide, behind the scenes state storage. This allows us to trigger an 
+  action in that component wide state storage, then directly pass that to the component that is interested
+  without building such long prop chains. */
+
 /* with const AuthContextProvider below, can actually import useState here inside of the 
 auth-context.js file. */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 /* On react, going to call createContext -> which creates a Context object */
 /* createContext takes a default context and Context here is just your App or component 
@@ -11,6 +23,8 @@ and set this to false initially. */
 /* What we get back from createContext is interesting. It will be a component/ or it will 
 an object that also contains components. AuthContext itself is not a component, it is an object
 that will contain a component. */
+/* We now have a stand alone file which manages the entire login state in the AuthContextProvider 
+component, and which also sets up all the context. */
 const AuthContext = React.createContext({
   isLoggedIn: false,
   /* good idea to add your functions like onLogout in my case to the default context when you 
@@ -23,21 +37,42 @@ const AuthContext = React.createContext({
 
 /* we are exporting AuthContextProvider component in addition to the default as a named export */
 export const AuthContextProvider = (props) => {
-  /* make auth-context stand alone file -> manages entire login state in AuthContextProvider component */
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  /* useEffect code needs to go into our AuthContextProvider */
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const logoutHandler = () => {
+    /* so here clear data in the logoutHandler */
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   const loginHandler = () => {
+    /* set our data in the login Handler */
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
   return (
+    //MAKING CONTENT DYNAMIC
+    /* We can set up dynamic context -> don't just pass data to our components but also functions. On 
+      AuthContext provider as well as passing down isLoggedIn, pass down onLogout -> can point at 
+      the logoutHandler */
+    /* If I do that, every listening component -> every component that listens to Auth context will be 
+      able to utilise logoutHandler simply through the onLogout context value. */
     <AuthContext.Provider
       value={{
+        /* isloggedIn = set to the isLoggedIn state */
         isLoggedIn: isLoggedIn,
+        /* onLogout -> points at the onLogoutHandler */
         onLogout: logoutHandler,
+        /* onLogin points at the loginHandler */
         onLogin: loginHandler,
       }}
     >

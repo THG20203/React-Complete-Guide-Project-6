700141db42b4f25f322f8645aa8e3e177c290a29
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-11 23:26:49 +0100

    making input reusable with props

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,17 +6,17 @@ const Input = (props) => {
     <div
       className={`${classes.control} ${
         /* instead of emailIsValid, we have emailState is valid */
-        emailState.isValid === false ? classes.invalid : ""
+        props.isValid === false ? classes.invalid : ""
       }`}
     >
-      <label htmlFor="email">E-Mail</label>
+      <label htmlFor={props.id}>{props.label}</label>
       <input
-        type="email"
-        id="email"
+        type={props.type}
+        id={props.id}
         /* below, where I pass the value back into the input, we can have emailState.value */
-        value={emailState.value}
-        onChange={emailChangeHandler}
-        onBlur={validateEmailHandler}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
       />
     </div>
   );
